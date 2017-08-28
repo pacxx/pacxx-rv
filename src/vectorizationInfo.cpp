146f101cd46959ea54652d1e15e2368d91273ae2
Lars Klein <l_klei14@wwu.de>
2017-08-28 16:53:35 +0200

    Revert "Revert "VI & VA : Initialize analysis explicitly via initialization list, overrides (pinned) now independent of initialization""
    This reverts commit bc2ba34d195e82768e0da1e33d294cc0be0ac1cf.

--- src/vectorizationInfo.cpp
+++ src/vectorizationInfo.cpp
@@ -120,7 +120,7 @@ VectorizationInfo::VectorizationInfo(llvm::Function& parentFn, uint vectorWidth,
     mapping.resultShape = VectorShape::uni();
     for (auto& arg : parentFn.args()) {
       mapping.argShapes.push_back(VectorShape::uni());
-      setVectorShape(arg, VectorShape::uni());
+      addInitial(&arg, VectorShape::uni());
     }
 }
 
@@ -132,7 +132,7 @@ VectorizationInfo::VectorizationInfo(VectorMapping _mapping)
   auto it = mapping.scalarFn->arg_begin();
   for (auto argShape : mapping.argShapes)
   {
-    setVectorShape(*it, argShape);
+    addInitial(&*it, argShape);
     ++it;
   }
 }
@@ -248,6 +248,32 @@ VectorizationInfo::isDivergentLoopTopLevel(const llvm::Loop* loop) const
 bool
 VectorizationInfo::isKillExit(const BasicBlock& BB) const {
     return NonKillExits.count(&BB) == 0;
+}
+
+bool VectorizationInfo::isPinned(const Value* V) {
+  return pinned.count(V) != 0;
+}
+
+void VectorizationInfo::setPinned(const Value* V) {
+  pinned.insert(V);
+}
+
+void VectorizationInfo::addInitial(const Value* V, VectorShape shape) {
+  initial[V] = shape;
+}
+
+void VectorizationInfo::addPinnedInitial(const Value* V, VectorShape shape) {
+  setPinned(V);
+  addInitial(V, shape);
+}
+
+std::map<const llvm::Value*, VectorShape>*
+VectorizationInfo::getInitialization() {
+  return &initial;
+}
+
+void VectorizationInfo::clearInitialization() {
+  initial.clear();
 }
 
 void
